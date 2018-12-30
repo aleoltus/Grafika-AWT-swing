fade2f9fb3cf5236307c7f81ecd6996e3ed29089
aleoltus <aleoltus@gmail.com>
2018-12-30 12:42:48 +0100

    #1 Hello Word

---
+++ MyFrame.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+public class MyFrame extends JFrame {
+
+	public MyFrame() {
+		super("Hello Word");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		pack();
+		setVisible(true);
+	}
+
+}
