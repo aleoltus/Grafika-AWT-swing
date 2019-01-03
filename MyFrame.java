797d240e017e69471cbd86fd0aab81b847b4fbce
aleoltus <aleoltus@gmail.com>
2019-01-03 11:48:13 +0100

    #7 resizable

--- MyFrame.java
+++ MyFrame.java
@@ -11,9 +11,10 @@ public class MyFrame extends JFrame {
 	
 		setLocation(600,400); //lokalizacjia
 		setSize(606,300); //rozmiar
+		setResizable(false); //nie mozna zmienic rozmiaru
 		
 		add(new JButton("Przycisk")); //przycisk
-		
+	
 		setVisible(true); 
 	}
 
