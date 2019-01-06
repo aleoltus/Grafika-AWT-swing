73807cc4e90f5dc18db645159c5c941a09bd0ea4
aleoltus <aleoltus@gmail.com>
2019-01-06 19:36:16 +0100

    #8 layout

--- MyFrame.java
+++ MyFrame.java
@@ -1,4 +1,5 @@
 import javax.swing.JFrame;
+import java.awt.FlowLayout;
 import javax.swing.JButton;
 
 
@@ -11,9 +12,12 @@ public class MyFrame extends JFrame {
 	
 		setLocation(600,400); //lokalizacjia
 		setSize(606,300); //rozmiar
-		setResizable(false); //nie mozna zmienic rozmiaru
+		//setResizable(false); //nie mozna zmienic rozmiaru
 		
-		add(new JButton("Przycisk")); //przycisk
+		setLayout(new FlowLayout());
+		
+		add(new JButton("Przycisk1"));
+		add(new JButton("Przycisk2"));
 	
 		setVisible(true); 
 	}
